914391a6bb2db7234228a3efb4664ea69b496710
sienaatl <info@sienaatl.com>
2026-07-01 12:16:09 -0400

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -13,12 +13,12 @@ async function verifyRecaptcha(token: string): Promise<boolean> {
 
 export async function POST(req: NextRequest) {
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: true,
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: false,
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user: "rsvp@sienaatl.com",
+      pass: "Devore124@",
     },
   });
 
